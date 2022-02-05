89a56de1501a4c11a9a67a119917698d8090d8c9
Fantazindy-KR <leehyen4965@gmail.com>
2022-02-05 15:20:38 +0900

    Add img preview and cancel

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,18 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 
 function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState();
   const inputRef = useRef();
-
-  // useEffect(() => {
-  //   if (inputRef.current) {
-  //     console.log(inputRef);
-  //   }
-  // }, []);
-
-  const handleChange = (e) => {
-    const nextValue = e.target.files[0];
-    onChange(name, nextValue);
-  };
 
   const handleClearClick = () => {
     const inputNode = inputRef.current;
@@ -22,9 +12,32 @@ function FileInput({ name, value, onChange }) {
     onChange(name, null);
   };
 
+  const handleChange = (e) => {
+    const nextValue = e.target.files[0];
+    onChange(name, nextValue);
+  };
+
+  useEffect(() => {
+    if (!value) return;
+
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview();
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value]);
+
   return (
     <div>
-      <input type="file" onChange={handleChange} ref={inputRef} />
+      <img src={preview} alt="이미지 미리보기" />
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={handleChange}
+        ref={inputRef}
+      />
       {value && <button onClick={handleClearClick}>X</button>}
     </div>
   );
