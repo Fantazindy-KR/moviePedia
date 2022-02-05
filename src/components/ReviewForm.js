fbf938e90ca24e1d27f71aaca5d024f8b9d2fbd3
Fantazindy-KR <leehyen4965@gmail.com>
2022-02-05 16:34:45 +0900

    Add Rating function by star

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import FileInput from "./FileInput";
+import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
 
 function ReviewFrom() {
@@ -35,11 +36,10 @@ function ReviewFrom() {
         onChange={handleChange}
       />
       <input name="title" value={values.title} onChange={handleInputChange} />
-      <input
-        type="number"
+      <RatingInput
         name="rating"
         value={values.rating}
-        onChange={handleInputChange}
+        onChange={handleChange}
       />
       <textarea
         name="content"
