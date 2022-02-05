3818c7cc5e70b6c186524b2af3a8ba623876dbc9
Fantazindy-KR <leehyen4965@gmail.com>
2022-02-05 17:06:22 +0900

    Now can post review to api server

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
+import { createReview } from "../api";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
 
+const INITIAL_VALUES = {
+  title: "",
+  rating: 0,
+  content: "",
+  imgFile: null,
+};
+
 function ReviewFrom() {
-  const [values, setValues] = useState({
-    title: "",
-    rating: 0,
-    content: "",
-    imgFile: null,
-  });
+  const [values, setValues] = useState(INITIAL_VALUES);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState(null);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -23,9 +28,25 @@ function ReviewFrom() {
     handleChange(name, value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("rating", values.rating);
+    formData.append("content", values.content);
+    formData.append("imgFile", values.imgFile);
+
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true);
+      await createReview(formData);
+    } catch (error) {
+      setSubmittingError(error);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
+    setValues(INITIAL_VALUES);
   };
 
   return (
@@ -46,7 +67,10 @@ function ReviewFrom() {
         value={values.content}
         onChange={handleInputChange}
       />
-      <button type="submit">확인</button>
+      <button disabled={isSubmitting} type="submit">
+        확인
+      </button>
+      {submittingError && <div>{submittingError.message}</div>}
     </form>
   );
 }
