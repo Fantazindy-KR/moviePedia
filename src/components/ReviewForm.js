65f568fbb5b45ef3b9af4e33b42ad595e58a5c1b
Fantazindy-KR <leehyen4965@gmail.com>
2022-02-07 18:09:17 +0900

    Create custom hook and remove dummy files

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import useAsync from "../hooks/useAsync";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
@@ -18,8 +19,7 @@ function ReviewFrom({
   onCancle,
 }) {
   const [values, setValues] = useState(initialValues);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submittingError, setSubmittingError] = useState(null);
+  const [isSubmitting, submittingError, onSubmitAsync] = useAsync(onSubmit);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -41,17 +41,9 @@ function ReviewFrom({
     formData.append("content", values.content);
     formData.append("imgFile", values.imgFile);
 
-    let result;
-    try {
-      setSubmittingError(null);
-      setIsSubmitting(true);
-      result = await onSubmit(formData);
-    } catch (error) {
-      setSubmittingError(error);
-      return;
-    } finally {
-      setIsSubmitting(false);
-    }
+    const result = await onSubmitAsync(formData);
+    if (!result) return;
+
     const { review } = result;
     setValues(INITIAL_VALUES);
     onSubmitSuccess(review);
